f2b3730011a7ed1becedbbe75bba9b9584981b35
david.stangeby <david.stangeby@e3f379d6-3178-d068-c8ba-cf1c5e729d0a>
2010-10-21 23:03:56 +0000

    Added DbTables, Models and Mappers for the current database layout.

---
+++ trunk/application/models/Employee.php
@@ -0,0 +1,12 @@
+<?php
+
+class Application_Model_Employee extends Tools_Model_Abstract
+{
+    protected $_id;
+    protected $_name;
+    protected $_username;
+    protected $_email;
+    protected $_password;
+    protected $_extra;
+    protected $_selected_id;
+}
---
+++ trunk/application/models/EmployeeMapper.php
@@ -0,0 +1,8 @@
+<?php
+
+class Application_Model_EmployeeMapper extends Tools_Model_Mapper_Abstract
+{
+    protected $defaultDbTable = 'Application_Model_DbTable_Employees';
+    protected $defaultModel = 'Application_Model_Employee';
+}
+
