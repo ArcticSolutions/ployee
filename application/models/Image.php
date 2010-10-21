ac0ecca97efd1fd61f34905705c6a63cad40962d
david.stangeby <david.stangeby@e3f379d6-3178-d068-c8ba-cf1c5e729d0a>
2010-10-21 23:03:56 +0000

    Added DbTables, Models and Mappers for the current database layout.

---
+++ application/models/Image.php
@@ -0,0 +1,9 @@
+<?php
+
+class Application_Model_Image extends Tools_Model_Abstract
+{
+    protected $_id;
+    protected $_filename;
+    protected $_emplyee_id;
+}
+
---
+++ application/models/ImageMapper.php
@@ -0,0 +1,8 @@
+<?php
+
+class Application_Model_ImageMapper extends Tools_Model_Mapper_Abstract
+{
+    protected $defaultDbTable = 'Application_Model_DbTable_Images';
+    protected $defaultModel = 'Application_Model_Image';
+}
+
