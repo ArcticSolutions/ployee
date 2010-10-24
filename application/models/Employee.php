0d389f72099ce19a4c81fcd345c318664a01c6f4
david.stangeby <david.stangeby@e3f379d6-3178-d068-c8ba-cf1c5e729d0a>
2010-10-24 08:09:29 +0000

    Set the $_extra variable to false as standard, not null.

--- application/models/Employee.php
+++ application/models/Employee.php
@@ -7,6 +7,72 @@ class Application_Model_Employee extends Tools_Model_Abstract
     protected $_username;
     protected $_email;
     protected $_password;
-    protected $_extra;
+    protected $_extra = false;
     protected $_selected_id;
+    
+    public function setName($name)
+    {
+        $this->_name = (string) $name;
+        return $this;
+    }
+    
+    public function getName()
+    {
+        return $this->_name;
+    }
+    
+    public function setUsername($username)
+    {
+        $this->_username = (string) $username;
+        return $this;
+    }
+    
+    public function getUsername()
+    {
+        return $this->_username;
+    }
+    
+    public function setEmail($email)
+    {
+        $this->_email = (string) $email;
+        return $this;
+    }
+    
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+    
+    public function setPassword($password)
+    {
+        $this->_password = (string) $password;
+        return $this;
+    }
+    
+    public function getPassword()
+    {
+        return $this->_password;
+    }
+    
+    public function setExtra($extra)
+    {
+        $this->_extra = (boolean) $extra;
+        return $this;
+    }
+    
+    public function getExtra()
+    {
+        return $this->_extra;
+    }
+    
+    public function setSelected_id($id)
+    {
+        $this->_selected_id = (integer) $id;
+        return $this;
+    }
+    
+    public function getSelected_id()
+    {
+        return $this->_selected_id;
+    }
 }
